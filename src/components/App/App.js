c5a6c6c721df403c50bc893e8a0580a78d2c82ea
thebrokenblow <artkrasov@gmail.com>
2023-08-21 17:14:27 +0300

    Added outputting list comix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
-import { API_URL, URL_COMICS } from '../../constants/api';
-import { getDataApi } from '../../utils/getDataApi';
+import Comics from '../Comics';
 
 import './App.css';
 
 class App {
     async render() {
-        const data = await getDataApi.getData(API_URL + URL_COMICS);
-        console.log(data);
+        await Comics.render();
     }
 }
 
---
+++ src/components/Comics/Comics.js
@@ -0,0 +1,47 @@
+import { API_URL, URL_COMICS, URL_CHARACTERS, IMG_STANDARD_XLARGE, IMG_NOT_AVAILABLE } from '../../constants/api';
+import { getDataApi } from '../../utils/getDataApi';
+import { ROOT_INDEX } from '../../constants/root';
+
+import './Comics.css';
+
+class Comics {
+    async render() {
+        const data = await getDataApi.getData(API_URL + URL_COMICS);
+
+        let htmlContent = '';
+        data.forEach(({ id, title, thumbnail: { path, extension } }) => {
+            
+            if (path.lastIndexOf(IMG_NOT_AVAILABLE) === -1) {
+                const uri = API_URL + URL_COMICS + '/' + id + '/' + URL_CHARACTERS;
+                const imgSrc = path + '/' + IMG_STANDARD_XLARGE + '.' + extension;
+
+                htmlContent += `
+                    <li class="comics__item" data-uri="${uri}">
+                        <span class="comics__name">${title}</span>
+                        <img class="comics__img" src="${imgSrc}" />
+                    </li>
+                `;
+            }
+        });
+
+        const htmlWrapper = `
+            <ul class="comics__container">
+                ${htmlContent}
+            </ul>
+        `;
+        console.log(htmlWrapper);
+        ROOT_INDEX.innerHTML = htmlWrapper;
+    }
+
+    eventListener() {
+        document.querySelectorAll('.comics__item').forEach(element => {
+            const uri = element.getAttribute('data-uri');
+
+            element.addEventListener('click', () => {
+                console.log(uri);
+            })
+        })
+    }
+}
+
+export default new Comics();
